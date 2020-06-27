04e2ce19eedd8e426c31c96618a08d538339cc2f
probison <parker.robison@hotmail.com>
2020-06-26 22:54:43 -0600

    added inquirer to index and added first question.

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,41 @@
 // array of questions for user
-const questions = [
+const questions = [ 
+    // 'What is the title of your project? (Required)',
+    // 'Provide a description of the project (Required)',
+    // 'Will your README have a Table of Contents?',
+    // 'How do you install the project?',
+    // 'Usage?',
+    // // this is a list of options
+    // 'License?',
+    // 'Contribution guidelines',
+    // 'Test instructions',
+   
 
 ];
 
+const inquirer = require('inquirer');
+
+const qPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name.')
+                }
+            }
+        },
+        {
+
+        }
+        
+
+    ])
+}
 // function to write README file
 function writeToFile(fileName, data) {
 }
@@ -14,3 +47,20 @@ function init() {
 
 // function call to initialize program
 init();
+
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
